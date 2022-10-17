cd5a3a3d8c4241761699e39c0c0ccd9ad3fc5c60
basharts <bashartukurshehu@gmail.com>
2022-10-16 23:34:56 -0700

    project 0x0A argc & argv task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,6 +22,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	num = atoi(argv[1]);
 	result = 0;
 
@@ -29,14 +30,16 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 		return (0);
-		for (j = 0; j < 5 && num >= 0; j++)
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			while (num >= coins[j])
-			{
-				result++;
-				num -= coins[j];
-			}
+			result++;
+			num -= coins[j];
 		}
-		printf("%d\n", result);
-		return (0);
+	}
+	printf("%d\n", result);
+	return (0);
 }

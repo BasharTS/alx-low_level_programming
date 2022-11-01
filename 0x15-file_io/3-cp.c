420af6298b9b9861515caf50c7e56bf1c7a4a7c8
basharts <bashartukurshehu@gmail.com>
2022-11-01 04:03:49 -0700

    bug fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,8 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 	return (buffer);
@@ -39,7 +40,8 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -64,7 +66,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -76,7 +79,7 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, 
+			dprintf(STDERR_FILENO,
 			"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
@@ -85,7 +88,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, 
+			dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);

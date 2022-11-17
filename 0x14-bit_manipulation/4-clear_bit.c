5d364107510b09eeb341df8a7cc716541804e0a2
basharts <bashartukurshehu@gmail.com>
2022-11-16 18:32:32 -0800

    Bug fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,6 @@
  * @index: index position to change
  * Return: 1 if it worked, -1 if error
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
@@ -15,10 +14,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 64)
 		return (-1);
-
 	hold = index;
-
-	for (i = 1; hold > 0; i *= 2, hold-)
+	for (i = 1; hold > 0; i *= 2, hold--)
 		;
 
 	if ((*n >> index) & 1)
